e1f3bfedc4f74acaa334a33fb84951d38ad18de5
milijanadj <milijana.djordjevic1998@gmail.com>
2021-12-09 18:55:42 +0100

    project initialized

---
+++ src/main/java/sep/secondbank/SecondBankApplication.java
@@ -0,0 +1,13 @@
+package sep.secondbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sep/secondbank/SecondBankApplicationTests.java
@@ -0,0 +1,13 @@
+package sep.secondbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
